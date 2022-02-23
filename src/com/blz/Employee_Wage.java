c3bb67b35e343e1b00a78f4e1ed6313006e328fd
LDeepakReddy <l.deepakreddy.com>
2022-02-23 20:20:38 +0530

    welcome to the Employee program

---
+++ src/com/blz/Employee_Wage.java
@@ -0,0 +1,8 @@
+package com.blz;
+
+public class Employee_Wage {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Employee wage computation program");
+    }
+}
