25dc41f8ba998feca7f09b6f8525d9a60be8d468
LDeepakReddy <l.deepakreddy.com>
2022-02-23 22:03:28 +0530

    Swith case

--- src/com/blz/Employee_Wage.java
+++ src/com/blz/Employee_Wage.java
@@ -1,31 +1,34 @@
 package com.blz;
 
 public class Employee_Wage {
+    public static final int IS_PRESENT = 1;
+    public static final int PARTIAL_PRESENT = 2;
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Employee wage computation program");
-        int Is_present = 1;
-        int PARTIAL_PRESENT = 2;
-        int WAGE_PER_HOUR = 8;
-        int FULLTIME_WORKINGHOUR = 20;
-        int PARTTIME_WORKINGHOUR = 4;
+
+        int WAGE_PER_HOUR = 20;
         int Employee_Wage = 0;
+        int EmpHr = 0;
 
 //Checking employee is present or absent
 
         int Empcheck = (int) (Math.random() * 10) % 3;
 
-        if (Empcheck == Is_present) {
-            System.out.println("Employee is present");
-            Employee_Wage = FULLTIME_WORKINGHOUR * WAGE_PER_HOUR;
-            System.out.println("Daily Employee Wage is " + Employee_Wage);
-        } else if (Empcheck == PARTIAL_PRESENT) {
-            System.out.println("Employee worked half day ");
-            Employee_Wage = PARTTIME_WORKINGHOUR * WAGE_PER_HOUR;
-            System.out.println("Part time employee wage per day is  " + Employee_Wage);
-        } else {
-            System.out.println("Employee is Absent");
-
+        switch (Empcheck) {
+            case IS_PRESENT:
+                System.out.println("Employee is present");
+                EmpHr = 8;
+                break;
+            case PARTIAL_PRESENT:
+                System.out.println("Employee is working part time");
+                EmpHr = 4;
+                break;
+            default:
+                System.out.println("Employee is absent");
         }
+
+        Employee_Wage = EmpHr * WAGE_PER_HOUR;
+        System.out.println("EMployee wage is " + Employee_Wage+ " Per day");
     }
 }
