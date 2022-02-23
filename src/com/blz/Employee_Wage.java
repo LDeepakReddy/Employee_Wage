8adb38cb1a042f7776ba6c19d9e7e978462e5e18
LDeepakReddy <l.deepakreddy.com>
2022-02-23 20:49:05 +0530

    check present or absent

--- src/com/blz/Employee_Wage.java
+++ src/com/blz/Employee_Wage.java
@@ -4,5 +4,14 @@ public class Employee_Wage {
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Employee wage computation program");
+//Checking employee is present or absent
+        int Is_present = 1;
+        int Empcheck = (int) (Math.random() * 10) % 2;
+
+        if (Empcheck == Is_present) {
+            System.out.println("Employee is present");
+        } else {
+            System.out.println("Employee is Absent");
+        }
     }
 }
